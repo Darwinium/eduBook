a2c132616dc2967d3e1e20c07d93ad4a01d92c2d
Ivo Dimitrov <darwinium@gmail.com>
2012-11-25 18:15:18 +0400

    Feed

--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,11 @@ Hrbot::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+
+  resources :products do
+    get :who_bought, on: :member
+  end
+
   root :to => 'store#index', as: 'store'
 
   # See how all your routes lay out with "rake routes"
